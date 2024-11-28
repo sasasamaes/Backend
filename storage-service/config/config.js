4ed4013ae7df5764adc1c239bec53248a0becb2a
Renzo Banegas <renzobanegas720@gmail.com>
2024-11-28 09:55:43 -0300

    feat(storage): add config file

---
+++ storage-service/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
+  R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
+  R2_ENDPOINT: process.env.R2_ENDPOINT,
+  R2_BUCKET: process.env.R2_BUCKET
+};
